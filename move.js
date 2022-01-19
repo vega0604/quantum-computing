c774c320481752709419ed86057ad64640066190
Sebastian Vega <sebasvega4604@gmail.com>
2022-01-19 10:20:17 -0500

    added smooth scroll

---
+++ move.js
@@ -0,0 +1,13 @@
+function move(id){
+    const element = document.querySelector(id);
+    const offset = 16 * 6;
+    const bodyRect = document.body.getBoundingClientRect().top;
+    const elementRect = element.getBoundingClientRect().top;
+    const elementPosition = elementRect - bodyRect;
+    const offsetPosition = elementPosition - offset;
+
+    window.scrollTo({
+    top: offsetPosition,
+    behavior: 'smooth'
+    });
+}
